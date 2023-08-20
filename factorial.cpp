02197cdda0bd789cf27fa310fbc4655bfb8f60aa
Snigdha02 <gkvsnigdha@gmail.com>
2023-08-20 17:10:54 +0530

    Modified source files

--- factorial.cpp
+++ factorial.cpp
@@ -3,7 +3,7 @@
 
 int factorial(int n){
     if (n<0){
-        std::cout << "Invalid number! Please re-enter a number greater than 0.";
+        std::cout << "Invalid number. Please re-enter a number greater than 0!";
         std::cout << std::endl;
     }	
     else if(n!=1){
