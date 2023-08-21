0742ef2d30200962fc2c3d7a2caabbdbe5f04899
Abhay <abhaygupta2479@gmail.com>
2023-08-21 14:47:53 +0530

    Check for negative numbers added

--- factorial.cpp
+++ factorial.cpp
@@ -5,6 +5,7 @@ int factorial(int n){
     if (n<0){
         printf("Invalid number. Please re-enter a number greater than 0!");
         printf("\n");
+        return -1;
     }	
     else if(n!=1){
 	return(n * factorial(n-1));
