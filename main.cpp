e1540a6b7709fbfe50bf299cacd7988cadae33b7
Saransh019 <b20132@students.iitmandi.ac.in>
2023-08-26 22:58:09 +0530

    recursive print statement added

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(){
     int x = 56, y=98;
     cout<<"GCD of x and y is "<<gcd(x,y)<<endl;
+    cout<<"GCD using recursive function : "<<gcdRec(x,y)<<endl;
     return 0;
 }
