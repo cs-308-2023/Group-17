88a67bf3782e86cf1e13c352bbf1b7efdba00d40
Saransh019 <b20132@students.iitmandi.ac.in>
2023-08-26 22:17:24 +0530

    gcd function created

--- factorial.cpp
+++ factorial.cpp
@@ -3,15 +3,9 @@
 using namespace std;
 int factorial(int n){
     if (n<0){
-<<<<<<< HEAD
         cout<<"Invalid number. Please re-enter a number greater than 0!");
         cout<<endl;
-=======
-        printf("Invalid number. Please re-enter a number greater than 0!");
-        printf("\n");
-        return -1;
->>>>>>> bugs-fixing
-    }	
+    }
     else if(n!=1){
 	return(n * factorial(n-1));
     }
